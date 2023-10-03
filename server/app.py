d5bbf17f43617330953d00f475490d05ea99c2dc
Kim Love <kimberlymlove15@gmail.com>
2023-10-03 17:49:02 -0400

    Code along complete

--- server/app.py
+++ server/app.py
@@ -16,17 +16,49 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    return make_response(
-        '<h1>Welcome to the pet directory!</h1>',
-        200
-    )
+    body = {"message": "Welcome to the pet directory!"}
+    return make_response(body, 200)
 
 
 @app.route('/demo_json')
 def demo_json():
-    pet_json = '{"id": 1, "name" : "Fido", "species" : "Dog"}'
-    return make_response(pet_json, 200)
+    pet = Pet.query.first()
+    pet_dict = {"id": pet.id, 
+                "name" : pet.name, 
+                "species" : pet.species
+            }
+    return make_response(pet_dict, 200)
 
+@app.route("/pets/<int:id>")
+def pet_by_id(id):
+    pet = Pet.query.filter_by(id=id).first()
+    if pet:
+        body = {
+            "id": pet.id,
+            "name": pet.name,
+            "species": pet.species
+        }
+        status_code = 200
+
+    else:
+        body = f"<p>There is not pet at {id}</p>"
+        status_code = 404
+    
+    return make_response(body, status_code)
+
+@app.route("/species/<string:species>")
+def get_pet_by_species(species):
+    pets= []
+
+    for pet in Pet.query.filter(Pet.species == species).all():
+        dict = {
+            "id": pet.id,
+            "name": pet.name,
+            "species": pet.species
+        }
+        pets.append(dict)
+
+    return make_response({"pets": pets}, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
